6b4bd7b31d25b32a2fb4d4ca7cc2c3086e9a8d3d
Muhammad Asif1163 <bibbasheikh11334@gmail.com>
2023-11-07 00:09:54 +0500

    2 files create

---
+++ app.js
@@ -0,0 +1,10 @@
+var myAge = ("Iam 15 years old" )
+var ageDays = 5475;
+var visited = ("You have visited this site 14 times")
+var visitTimes = 14
+
+
+alert(myAge)
+alert(ageDays)
+alert(visited)
+alert(visitTimes)
